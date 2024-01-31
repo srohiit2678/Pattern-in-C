a520f2e24072207a57018ec3a997afb937f52993
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-01 00:16:28 +0530

    Revert "Update Pattern1.c"
    This reverts commit 8394e50c7474ded1a7036b7311762791457a6341.

--- Pattern1.c
+++ Pattern1.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 int main(){
- for (int i = 0; i <= 5; i++)
-  {
-    for (int j = 0; j <= 5; j++)
-        {
-             printf("* ");
-        }
-       printf("\n");
-  }
+    for (int i = 0; i < 5; i++)
+{
+    printf("%d",i);
+}
 return 0;
 }
