8394e50c7474ded1a7036b7311762791457a6341
ROHIT KUMAR OJHA <143264999+srohiit2678@users.noreply.github.com>
2024-02-01 00:09:15 +0530

    Update Pattern1.c

--- Pattern1.c
+++ Pattern1.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main(){
-    for (int i = 0; i < 5; i++)
-{
-    printf("%d",i);
-}
+ for (int i = 0; i <= 5; i++)
+  {
+    for (int j = 0; j <= 5; j++)
+        {
+             printf("* ");
+        }
+       printf("\n");
+  }
 return 0;
 }
