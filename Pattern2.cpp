f2de26c6324537bf1090af670e2f14eb426308b9
ROHIT KUMAR OJHA <143264999+srohiit2678@users.noreply.github.com>
2024-02-01 00:29:37 +0530

    Update Pattern2.cpp
    added

--- Pattern2.cpp
+++ Pattern2.cpp
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+  for (int i = 1; i <= 5; i++)
+   { 
+     for (int j = 1; j <= 5; j++)
+      {
+        printf("%d",i);
+      }
+        printf("\n");
+   }
+return 0;
+}
