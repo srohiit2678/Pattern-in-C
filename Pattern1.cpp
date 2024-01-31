4e06b542e9c6382e63a8dab235b2bed4c993b373
ROHIT KUMAR OJHA <143264999+srohiit2678@users.noreply.github.com>
2024-02-01 00:25:25 +0530

    Create Pattern1.cpp
    fill added.

---
+++ Pattern1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  for (int i = 0; i < 5; i++)
+   { 
+     for (int i = 0; i < 5; i++)
+      {
+        printf("%d",i);
+      }
+        printf("\n");
+   }
+return 0;
+}
