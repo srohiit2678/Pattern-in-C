c778e7ce65b62bcc0b41cd796e8fa38f0183cec8
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-01 16:36:47 +0530

    add
    new pattern.

---
+++ Pattern7.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int is_even(int k){
+  if(k%2!=0){
+    return k;
+  }
+  else{
+    while(k%2==0)
+    {
+      k++;
+    }
+    return k;
+  }
+}
+int main()
+{
+  int k=1,n = 5;
+  for(int i = 1; i <= n; i++)
+    {
+      for(int j = 1; j <= n; j++)
+        {
+          {
+            printf("%3d",k=is_even(k)); // --> %3d = here 3 is widht of element with space (_345 = %4d)
+         
+          }
+          k++;
+      }
+      printf("\n");
+}
+return 0;
+}
