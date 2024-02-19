d01e00926d454d832458991d78d8f4ee05412a5d
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-19 16:06:36 +0530

    19-2-24
    numbers  pattern

--- Patterns.c
+++ Patterns.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 int main(){
 
- 
+
+int n = 5;
+  for(int i = 1; i <= n; i++)
+    {
+      for(int j = 1; j <= 3; j++)
+        {
+            printf("%d %d ",i,j);
+                    
+      }
+      printf("\n");
+}
 
 
 return 0;
