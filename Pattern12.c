d79a0419f15ece82d91c8856684b1ed19781b617
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-19 16:31:39 +0530

    19-2-24
    new number patter

---
+++ Pattern12.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+// 2 logic
+
+int main(){
+
+int n = 5 ,k;
+  for(int i = 1; i <= n; i++)
+    {
+          k=i;
+
+      for(int j = 1; j <= n; j++)
+        {
+
+            printf("%3d",k);
+              k+=n;
+      }
+      printf("\n");
+}
+
+return 0;
+}
+
+/*
+---> 2nd logic <-----
+
+int n = 5 ,k;
+  for(int i = 1; i <= n; i++)
+    {
+          k=i;
+
+      for(int j = i; j <n+i; j++)
+        {
+
+            printf("%3d",k);
+              k+=n;
+      }
+      printf("\n");
+}
+
+
+*/
--- Patterns.c
+++ Patterns.c
@@ -1,21 +1,59 @@
 #include<stdio.h>
+// 2 logic
+
 int main(){
 
-
-int n = 5;
+int n = 5 ,k;
   for(int i = 1; i <= n; i++)
     {
-      for(int j = 1; j <= 3; j++)
+          k=i;
+
+      for(int j = 1; j <= n; j++)
         {
-            printf("%d %d ",i,j);
-                    
+
+            printf("%3d",k);
+              k+=n;
+      }
+      printf("\n");
+}
+
+return 0;
+}
+
+/*
+---> 2nd logic <-----
+
+int n = 5 ,k;
+  for(int i = 1; i <= n; i++)
+    {
+          k=i;
+
+      for(int j = i; j <n+i; j++)
+        {
+
+            printf("%3d",k);
+              k+=n;
       }
       printf("\n");
 }
 
 
-return 0;
-}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // int main()
