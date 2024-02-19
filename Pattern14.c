c9a3489c7a4cd6215feb0d8b3d236688f927f896
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-19 18:05:25 +0530

    19-2-24
    number patter added

---
+++ Pattern14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+// 2 logic
+
+int main(){
+
+int n = 5 ,k;
+  for(int i = n; i >= 1; i--)
+    {
+        k= i;
+
+      for(int j = 1; j <n; j++)
+        {
+            printf("%3d",k);
+
+            k += n;    
+      }
+      printf("\n");
+}
+
+return 0;
+}
