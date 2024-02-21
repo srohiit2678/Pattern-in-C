4a40cb69d88344683bd75b06d34b133b3a7cb95a
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-22 01:45:48 +0530

    22-2-24
    number pattern added

---
+++ Pattern18.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main(){
+    
+    int i,j;
+    int k=0,n=5;
+
+    for(i=1;i<=n;i++){
+        
+        for(j=1;j<=n;j++){
+          printf("%2d ",k%2);
+          k++;
+        }
+      printf("\n"); 
+    }
+}
