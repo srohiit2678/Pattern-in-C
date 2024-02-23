0e0d3fb51d3b9acc4fbe5f0aae533f34df9431d4
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-24 02:32:47 +0530

    24-02-24
    number patterns added

---
+++ Pattern37.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i,j;
+    int n=5;
+    for(i=n;i>=1;i--){
+      for(j=5;j>=i;j--){
+          printf("%d ",i);        
+      }
+      printf("\n");
+    } 
+}
