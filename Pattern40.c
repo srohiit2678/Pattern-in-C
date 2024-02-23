253ff07a64daf9b4bd3b81a691632af002a6b538
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-02-24 02:33:54 +0530

    24-2-24
    number patterns added

---
+++ Pattern40.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, j;
+  int n = 5;
+  for (i = 1; i <= n; i++)
+  {
+    for (j =1; j<=i; j++)
+    {
+      printf("%d ", 2*j);
+    }
+    printf("\n");
+  }
+}
