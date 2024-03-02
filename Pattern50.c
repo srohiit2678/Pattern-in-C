97abda84d15424e71f6153bff1561b6657d1ee2d
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-03-02 16:59:03 +0530

    2-3-24
    numbers pattern

---
+++ Pattern50.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+  int n=5;
+ 
+  int x=1,y=n;
+  int p,q,r,s;
+ 
+  for (int i=n;i>=1;i--)
+  {    p = x;
+       q = y;
+       r = i;
+       s = i+1;
+    for (int j=n;j>=i;j--)
+    {     
+       if(((i+j)%2 == 1)){
+        printf("%2d ",p);
+       }
+       else
+       {
+        printf("%2d ",q);
+       }
+       p = p - r++;
+       q = q - s++;
+    }
+      y = y+i-1;
+      x = x+i;
+    printf("\n");
+  }
+}
