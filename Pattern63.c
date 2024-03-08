a979279112f10820413cb6cd77e4c15321f98b60
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-03-08 12:21:38 +0530

    8-3-24
    number pattern

---
+++ Pattern63.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int n=5;
+  int p;
+  for(int i=0;i<=n-1;i++)
+  {  p=i;
+    for(int j=0;j<=i;j++)
+    {         
+         printf("%d ",p%2); p++;
+    }
+    printf("\n");
+  }
+}
