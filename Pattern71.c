c76331f0e4eea24ab10c380afde67f9183d2b7b5
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-08 11:48:47 +0530

    Create Pattern71.c

---
+++ Pattern71.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int n = 5;
+  for (int i= 1; i <= n; i++)
+  { 
+    for (int j = n; j>= i; j--)
+    {
+      printf("%d ",i);
+    }
+   
+    printf("\n");
+  }
+}
