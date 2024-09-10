e82adb7d6235282e2931c428f6eba09b999ae101
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-10 19:27:51 +0530

    Create Pattern76.c

---
+++ Pattern76.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int n = 5;
+  int p=5;
+  for (int i= n; i >=1 ; i--)
+  { 
+    for (int j = 0; j<=i-1; j++)
+    {
+      printf("%d ",p+j);
+    }
+     p--;
+    printf("\n");
+  }
+}
