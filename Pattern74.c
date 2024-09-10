59f39a88309a907889e22436718a9d58b83a9cd7
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-10 19:27:17 +0530

    Create Pattern74.c

---
+++ Pattern74.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int n = 5;
+  for (int i= 1; i <n ; i++)
+  { 
+    for (int j = 5; j>=i; j--)
+    {
+      printf("%d ",j);
+    }
+   
+    printf("\n");
+  }
+}
