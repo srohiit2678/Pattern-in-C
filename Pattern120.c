408493ce482658e5310a09c7467cd0e233722b92
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-06 02:49:57 +0530

    Create Pattern120.c

---
+++ Pattern120.c
@@ -0,0 +1,26 @@
+/*
+
+  1 1 1 1 1      
+    2 2 2 2      
+      3 3 3      
+        4 4      
+          5  
+*/
+#include<stdio.h>
+int main()
+{
+  int n = 5;
+  printf("\n");   
+  for (int i=1; i <= n ; i++)
+  {      
+    for (int k=1 ;k<=i; k++) // for space
+    { 
+      printf("  "); 
+    }    
+    for (int j = 1; j<= (n+1)-i; j++) // for pattern
+    {    
+        printf("%d ",i);
+    }    
+     printf("\n");
+    }              
+  }
